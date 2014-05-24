efd6ccf6aa8ad11e9bb2fe6561b39c380365f53b
Theo Pack <tf.pack@gmail.com>
2014-05-24 15:32:51 +0200

    add first sldies

---
+++ slides/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'MongoDB<br>Workshop',
+    subtitle: 'An open-source, document-oriented database',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  presenters: [{
+    name: 'Theo Pack',
+    company: '',
+    gplus: '',
+    twitter: '@furikuri',
+    www: 'http://furikuri.github.io',
+    github: 'http://github.com/furikuri'
+  }]
+};
+
